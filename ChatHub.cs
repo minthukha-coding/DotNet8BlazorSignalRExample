00752384dfed80b2cc83d839d25475ffac0b73cd
minthukha <minthukha1239788@gmail.com>
2025-03-17 14:27:31 +0630

    Add project files.

---
+++ ChatHub.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNet8BlazorSignalRExample;
+
+using Microsoft.AspNetCore.SignalR;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task NotifyTyping(string user)
+    {
+        await Clients.Others.SendAsync("UserTyping", user);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using DotNet8BlazorSignalRExample;
+using DotNet8BlazorSignalRExample.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<ChatHub>("/chatHub");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
